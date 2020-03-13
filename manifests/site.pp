98d5ed2d79f524f33f933790e3e1f98a448f49ec
Luis Fernando Salazar Rivera <lfsalazarrivera@gmail.com>
2020-03-13 17:30:23 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,19 @@
 node default {
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner => 'root';
+  }
 }
 node 'server1.eproseedgt.com'{
   include role::master_server
 }
+
+node /̣^̣web/ {
+  include role::app_server
+}
+
+node /̣^̣db/ {
+  include role::db_server
+}
+
