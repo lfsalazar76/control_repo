0dba184a87b3ec38eb9ddfdec21a31c107207a48
Luis Fernando Salazar Rivera <lfsalazarrivera@gmail.com>
2020-03-13 18:29:44 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node{'web.eproseedgt.com':}
   dockeragent::node{'db.eproseedgt.com':}
+  host {'web.eproseedgt.com':
+    ensure => present,
+    ip     => '172.18.0.2'
+  }
+  host {'db.eproseedgt.com':
+    ensure => present,
+    ip     => '172.18.0.3'
+  }
 }
